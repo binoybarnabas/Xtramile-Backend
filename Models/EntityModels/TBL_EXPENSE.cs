c27e7cb41014c52711842422a621c9beb4d5c631
Binoy Barnabas <binoy.barnabas@experionglobal.local>
2024-01-28 12:41:44 +0530

    feat: added api to list invoices

--- Models/EntityModels/TBL_EXPENSE.cs
+++ Models/EntityModels/TBL_EXPENSE.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace XtramileBackend.Models.EntityModels
@@ -7,13 +8,15 @@ namespace XtramileBackend.Models.EntityModels
     public class TBL_EXPENSE
     {
         [Key]
-        public int UtrId { get; set; }
-        public int? InvoiceId { get; set; }
+        public int ExpenseId { get; set; }
+        public string? UtrId { get; set; }
+        public int InvoiceId { get; set; }
         public string? Description { get; set; }
         public string VendorName { get; set; }
         public string? VendorContact { get; set; }
         public string? VendorEmail { get; set; }
         public double? InvoiceAmount { get; set; }
+        public DateTime? PaidOn { get; set; }
 
     }
 }
