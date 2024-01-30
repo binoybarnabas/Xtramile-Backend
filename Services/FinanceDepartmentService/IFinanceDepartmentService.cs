3c07e61182370ad725eb974aa466c74826637374
Binoy Barnabas <binoy.barnabas@experionglobal.local>
2024-01-30 23:46:53 +0530

    fix: expense, invoice status and invoice attachement tables fix

--- Services/FinanceDepartmentService/IFinanceDepartmentService.cs
+++ Services/FinanceDepartmentService/IFinanceDepartmentService.cs
@@ -8,5 +8,7 @@ namespace XtramileBackend.Services.FinanceDepartment
         public Task<IEnumerable<FinanceRequest>> SortIncomingList(string sortField, bool isDescending);
 
         public Task<IEnumerable<InvoiceAttachment>> GetAllInvoiceAttachments();
+        public Task<Boolean> UpdateInvoiceStatus(int InvoiceId, InvoiceStatus invoiceStatus);
+        public Task<IEnumerable<InvoiceAttachment>> GetInvoicesBasedOnStatus(bool IsUtr);
     }
 }
