3e0ec6dfc005b5f932003c900bd390e557ac2828
Binoy Barnabas <binoy.barnabas@experionglobal.local>
2024-01-31 15:45:26 +0530

    feat: ongoing trips

--- Controllers/TravelAdminControllers/TravelAdminController.cs
+++ Controllers/TravelAdminControllers/TravelAdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using XtramileBackend.Services.TravelAdminService;
 
 namespace XtramileBackend.Controllers.TravelAdminControllers
 {
@@ -7,7 +8,23 @@ namespace XtramileBackend.Controllers.TravelAdminControllers
     [ApiController]
     public class TravelAdminController : ControllerBase
     {
+        private readonly ITravelAdminService _travelAdminService;
+        public TravelAdminController(ITravelAdminService travelAdminService) {
+            _travelAdminService = travelAdminService;
+        }
 
-       
+        [HttpGet("ongoing")]
+        public async Task<IActionResult> GetOnGoingTravel()
+        {
+            try {
+                var onGoingTravelData = await _travelAdminService.OnGoingTravel();
+                return Ok(onGoingTravelData);
+            }
+            catch (Exception ex) {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred while getting on going travel updates: {ex.Message}");
+            }
+
+        }
+
     }
 }
