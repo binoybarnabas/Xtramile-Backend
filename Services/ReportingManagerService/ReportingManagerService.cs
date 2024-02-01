3e3f4c6eaa679af6f372a3b4210ce3d99bb94e02
Bivina M V <bivina.mv@experionglobal.local>
2024-02-01 13:01:26 +0530

    fix: changed linq query based on changes in the table

--- Services/ReportingManagerService/ReportingManagerService.cs
+++ Services/ReportingManagerService/ReportingManagerService.cs
@@ -2,11 +2,11 @@
 using Microsoft.EntityFrameworkCore;
 using XtramileBackend.Data;
 using XtramileBackend.Models.APIModels;
+using XtramileBackend.Models.EntityModels;
 
 namespace XtramileBackend.Services.ManagerService
 {
-
-    public class ReportingManagerService
+    public class ReportingManagerService: IReportingManagerService
     {
         private readonly AppDBContext _context;
         public ReportingManagerService(AppDBContext DBContext)
@@ -17,20 +17,21 @@ namespace XtramileBackend.Services.ManagerService
         {
             try
             {
-                var EmpRequest = await (
+               var EmpRequest = await (
                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-               join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
+               join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+               join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
                where TBL_EMPLOYEE.ReportsTo == managerId
                select new EmployeeRequestDto
                {
                    RequestId = TBL_REQUEST.RequestId,
-                   EmployeeName = TBL_EMPLOYEE.FirstName + "  " +TBL_EMPLOYEE.LastName,
+                   EmployeeName = TBL_EMPLOYEE.FirstName + TBL_EMPLOYEE.LastName,
                    Email = TBL_EMPLOYEE.Email,
                    ProjectCode = TBL_PROJECT.ProjectCode,
-                   Date = TBL_REQUEST.CreatedOn,  // Assuming TBL_REQUEST has a 'date' property
-                   Mode = null,  // You need to adjust this based on your data model
-                   Status = null  // You need to adjust this based on your data model
+                   Date = TBL_REQUEST.CreatedOn,  
+                   Mode = null,  
+                   Status = null  
                }).ToListAsync();
 
                 return EmpRequest;
@@ -42,17 +43,25 @@ namespace XtramileBackend.Services.ManagerService
 
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <returns></returns>
 
+
+        // Get employee requests sorted by requestid
         public async Task<List<EmployeeRequestDto>> GetEmployeeRequestsSortByRequestCodeAsync([FromQuery] int managerId)
         {
             try
             {
                 var EmpRequest = await (
-               from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
-               join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-               join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
-               where TBL_EMPLOYEE.ReportsTo == managerId
-               orderby TBL_REQUEST.RequestCode
+                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
+                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
+                join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+                join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
+                where TBL_EMPLOYEE.ReportsTo == managerId
+                orderby TBL_REQUEST.RequestId
                select new EmployeeRequestDto
                {
                    RequestId = TBL_REQUEST.RequestId,
@@ -73,7 +82,14 @@ namespace XtramileBackend.Services.ManagerService
 
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <returns></returns>
+        /// 
 
+        // Get employee requests sorted by email
         public async Task<List<EmployeeRequestDto>> GetEmployeeRequestsSortByEmailAsync([FromQuery] int managerId)
         {
             try
@@ -81,7 +97,8 @@ namespace XtramileBackend.Services.ManagerService
                 var EmpRequest = await (
                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-               join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
+               join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+               join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
                where TBL_EMPLOYEE.ReportsTo == managerId
                orderby TBL_EMPLOYEE.Email
                select new EmployeeRequestDto
@@ -90,9 +107,9 @@ namespace XtramileBackend.Services.ManagerService
                    EmployeeName = TBL_EMPLOYEE.FirstName + TBL_EMPLOYEE.LastName,
                    Email = TBL_EMPLOYEE.Email,
                    ProjectCode = TBL_PROJECT.ProjectCode,
-                   Date = TBL_REQUEST.CreatedOn,  // Assuming TBL_REQUEST has a 'date' property
-                   Mode = null,  // You need to adjust this based on your data model
-                   Status = null  // You need to adjust this based on your data model
+                   Date = TBL_REQUEST.CreatedOn,  
+                   Mode = null,  
+                   Status = null  
                }).ToListAsync();
 
                 return EmpRequest;
@@ -104,7 +121,15 @@ namespace XtramileBackend.Services.ManagerService
 
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <returns></returns>
+        /// 
 
+
+        // Get employee requests sorted by date
         public async Task<List<EmployeeRequestDto>> GetEmployeeRequestsSortByDateAsync([FromQuery] int managerId)
         {
             try
@@ -112,7 +137,8 @@ namespace XtramileBackend.Services.ManagerService
                 var EmpRequest = await (
                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-               join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
+               join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+               join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
                where TBL_EMPLOYEE.ReportsTo == managerId
                orderby TBL_REQUEST.CreatedOn
                select new EmployeeRequestDto
@@ -121,9 +147,9 @@ namespace XtramileBackend.Services.ManagerService
                    EmployeeName = TBL_EMPLOYEE.FirstName + TBL_EMPLOYEE.LastName,
                    Email = TBL_EMPLOYEE.Email,
                    ProjectCode = TBL_PROJECT.ProjectCode,
-                   Date = TBL_REQUEST.CreatedOn,  // Assuming TBL_REQUEST has a 'date' property
-                   Mode = null,  // You need to adjust this based on your data model
-                   Status = null  // You need to adjust this based on your data model
+                   Date = TBL_REQUEST.CreatedOn,  
+                   Mode = null,  
+                   Status = null  
                }).ToListAsync();
 
                 return EmpRequest;
@@ -135,26 +161,35 @@ namespace XtramileBackend.Services.ManagerService
 
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <param name="date"></param>
+        /// <returns></returns>
 
+
+        // Get employee requests for a specific date
         public async Task<List<EmployeeRequestDto>> GetEmployeeRequestsByDateAsync([FromQuery] int managerId, DateTime date)
         {
 
             try
             {
                 var EmpRequest = await (
-                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
-                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-                join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
-                where TBL_EMPLOYEE.ReportsTo == managerId & TBL_REQUEST.CreatedOn.Date == date.Date
+               from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
+               join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
+               join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+               join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
+               where TBL_EMPLOYEE.ReportsTo == managerId & TBL_REQUEST.CreatedOn.Date == date.Date
                 select new EmployeeRequestDto
                 {
                     RequestId = TBL_REQUEST.RequestId,
                     EmployeeName = TBL_EMPLOYEE.FirstName + " " + TBL_EMPLOYEE.LastName,
                     Email = TBL_EMPLOYEE.Email,
                     ProjectCode = TBL_PROJECT.ProjectCode,
-                    Date = TBL_REQUEST.CreatedOn,  // Assuming TBL_REQUEST has a 'date' property
-                    Mode = null,  // You need to adjust this based on your data model
-                    Status = null  // You need to adjust this based on your data model
+                    Date = TBL_REQUEST.CreatedOn,  
+                    Mode = null,  
+                    Status = null 
                 }).ToListAsync();
 
                 return EmpRequest;
@@ -168,24 +203,35 @@ namespace XtramileBackend.Services.ManagerService
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="managerId"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        /// 
+        
+        
+        // Get employee requests for a specific email
         public async Task<List<EmployeeRequestDto>> GetEmployeeRequestsByEmailAsync([FromQuery] int managerId, string email)
         {
             try
             {
                 var EmpRequest = await (
-                from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
-                join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
-                join TBL_PROJECT in _context.TBL_PROJECT on TBL_REQUEST.ProjectId equals TBL_PROJECT.ProjectId
-                where TBL_EMPLOYEE.ReportsTo == managerId & TBL_EMPLOYEE.Email == email
+               from TBL_EMPLOYEE in _context.TBL_EMPLOYEE
+               join TBL_REQUEST in _context.TBL_REQUEST on TBL_EMPLOYEE.EmpId equals TBL_REQUEST.CreatedBy
+               join TBL_PROJECT_MAPPING in _context.TBL_PROJECT_MAPPING on TBL_EMPLOYEE.EmpId equals TBL_PROJECT_MAPPING.EmpId
+               join TBL_PROJECT in _context.TBL_PROJECT on TBL_PROJECT_MAPPING.ProjectId equals TBL_PROJECT.ProjectId
+               where TBL_EMPLOYEE.ReportsTo == managerId && TBL_EMPLOYEE.Email == email
                 select new EmployeeRequestDto
                 {
                     RequestId = TBL_REQUEST.RequestId,
                     EmployeeName = TBL_EMPLOYEE.FirstName + " " + TBL_EMPLOYEE.LastName,
                     Email = TBL_EMPLOYEE.Email,
                     ProjectCode = TBL_PROJECT.ProjectCode,
-                    Date = TBL_REQUEST.CreatedOn,  // Assuming TBL_REQUEST has a 'date' property
-                    Mode = null,  // You need to adjust this based on your data model
-                    Status = null  // You need to adjust this based on your data model
+                    Date = TBL_REQUEST.CreatedOn, 
+                    Mode = null, 
+                    Status = null  
                 }).ToListAsync();
 
                 return EmpRequest;
