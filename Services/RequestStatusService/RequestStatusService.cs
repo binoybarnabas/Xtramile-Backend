73c63b89c4415ba549baf3487730091a19d2366c
Abhinav Nair A S <abhinav.as@experionglobal.local>
2024-02-02 11:54:23 +0530

    Rebase

--- Services/RequestStatusService/RequestStatusService.cs
+++ Services/RequestStatusService/RequestStatusService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using XtramileBackend.Models.APIModels;
 using XtramileBackend.Models.EntityModels;
 using XtramileBackend.UnitOfWork;
 
@@ -41,6 +42,29 @@ namespace XtramileBackend.Services.RequestStatusService
             {
                 // Handle or log the exception
                 Console.WriteLine($"An error occurred while adding a request status: {ex.Message}");
+                throw; // Re-throw the exception to propagate it
+            }
+        }
+        public async Task<string> GetRequestStatusNameAsync(int requestId)
+        {
+            try
+            {
+                IEnumerable<TBL_REQ_APPROVE> statusApprovalMap = await _unitOfWork.RequestStatusRepository.GetAllAsync();
+                IEnumerable<TBL_STATUS> statusData = await _unitOfWork.StatusRepository.GetAllAsync();
+
+                var result = (from statusApproval in statusApprovalMap
+                              join status in statusData on statusApproval.PrimaryStatusId equals status.StatusId
+                              where statusApproval.RequestId == requestId
+                              select new PendingRequetsViewEmployee
+                              {
+                                  statusName = status.StatusName
+                              }).LastOrDefault();
+                return result?.statusName ?? "undefined";
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception
+                Console.WriteLine($"An error occurred while getting pending requests: {ex.Message}");
                 throw; // Re-throw the exception to propagate it
             }
         }
