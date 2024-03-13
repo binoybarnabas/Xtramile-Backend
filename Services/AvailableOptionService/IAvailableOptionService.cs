afc4328f520677bf09513c64f4be477050425dad
Binoy Barnabas <binoy.barnabas@experionglobal.local>
2024-03-13 18:07:50 +0530

    feat: added controllers and service to get all the available options in text form based in request ids

--- Services/AvailableOptionService/IAvailableOptionService.cs
+++ Services/AvailableOptionService/IAvailableOptionService.cs
@@ -9,7 +9,7 @@ namespace XtramileBackend.Services.AvailableOptionService
         public Task AddAvailableOptionAsync(TBL_AVAIL_OPTION availableOption);
         public Task<int> AddNewTravelOptionAsync(TBL_TRAVEL_OPTION travelOption);
         public Task UpdateFileIdOfOptionAsync(int fileId, int optionId);
-        public Task<IEnumerable<TBL_TRAVEL_OPTION>> GetTravelOptionsByRequestIdAsync(int reqId);
+        public Task<IEnumerable<TBL_TRAVEL_OPTION>> GetTravelOptionsByRequestIdAsync(int reqId,bool travelOption);
         public Task<string> AddAvailableTextOptionAsync(AvailableOption availableOption);
 
     }
