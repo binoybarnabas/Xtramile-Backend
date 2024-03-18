abc1a1c771743a233c6d5442d0ebc60542673f57
Abhinav Nair A S <abhinav.as@experionglobal.local>
2024-03-18 11:23:36 +0530

    Fix: Completed Trips Employee Dashboard Component

--- Models/APIModels/TravelDocument.cs
+++ Models/APIModels/TravelDocument.cs
@@ -3,7 +3,7 @@
     public class TravelDocument
     {
         public string Country { get; set; }
-        public DateTime ExpiryDate { get; set; }
+        public DateTime? ExpiryDate { get; set; }
         public IFormFile File { get; set; }
         public string TravelDocType { get; set; }
         public int UploadedBy { get; set; }
